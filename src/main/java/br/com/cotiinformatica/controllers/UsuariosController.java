11095cc5d94c39e5537ff4aad88bbf9fd13efcb1
alison <adevpr11@gmail.com>
2025-06-12 13:00:00 -0300

    feat: adiciona UsuariosController

---
+++ src/main/java/br/com/cotiinformatica/controllers/UsuariosController.java
@@ -0,0 +1,27 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cotiinformatica.dtos.CriarUsuarioRequestDTO;
+import br.com.cotiinformatica.dtos.CriarUsuarioResponseDTO;
+import br.com.cotiinformatica.services.UsuarioService;
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+public class UsuariosController {
+
+	private UsuarioService usuarioService;
+
+	public UsuariosController(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	@PostMapping
+	public CriarUsuarioResponseDTO criar(@RequestBody CriarUsuarioRequestDTO requestDTO) {
+
+		return usuarioService.criarUsuario(requestDTO);
+	}
+}
