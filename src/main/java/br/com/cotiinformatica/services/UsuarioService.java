9560eef4b40ac979f3c2f8af7a1098c80de942cd
alison <adevpr11@gmail.com>
2025-06-17 12:53:47 -0300

    feat: adiciona método autenticarUsuario em UsuarioService

--- src/main/java/br/com/cotiinformatica/services/UsuarioService.java
+++ src/main/java/br/com/cotiinformatica/services/UsuarioService.java
@@ -5,9 +5,12 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 
 import br.com.cotiinformatica.components.CryptoComponent;
+import br.com.cotiinformatica.dtos.AutenticarUsuarioRequestDTO;
+import br.com.cotiinformatica.dtos.AutenticarUsuarioResponseDTO;
 import br.com.cotiinformatica.dtos.CriarUsuarioRequestDTO;
 import br.com.cotiinformatica.dtos.CriarUsuarioResponseDTO;
 import br.com.cotiinformatica.entities.Usuario;
+import br.com.cotiinformatica.exceptions.AcessoNegadoException;
 import br.com.cotiinformatica.exceptions.EmailJaCadastradoException;
 import br.com.cotiinformatica.repositories.UsuarioRepository;
 
@@ -40,6 +43,25 @@ public class UsuarioService {
 		responseDTO.setNome(usuario.getNome());
 		responseDTO.setEmail(usuario.getEmail());
 		responseDTO.setDataHoraCriacao(LocalDateTime.now());
+		return responseDTO;
+	}
+	
+	public AutenticarUsuarioResponseDTO autenticarUsuario(AutenticarUsuarioRequestDTO requestDTO) {
+
+		var usuario = usuarioRepository.find(requestDTO.getEmail(),
+				cryptoComponent.getSHA256(requestDTO.getSenha()));
+
+		if (usuario == null) {
+			throw new AcessoNegadoException();
+		}
+
+		var responseDTO = new AutenticarUsuarioResponseDTO();
+		responseDTO.setId(usuario.getId());
+		responseDTO.setNome(usuario.getNome());
+		responseDTO.setEmail(usuario.getEmail());
+		responseDTO.setDataHoraAcesso(LocalDateTime.now());
+		responseDTO.setToken("token-gerado-aqui"); 
+
 		return responseDTO;
 	}
 }
