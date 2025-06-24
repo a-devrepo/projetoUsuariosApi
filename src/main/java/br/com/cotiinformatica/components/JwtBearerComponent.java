2d42ce5a0714f9b093f85a565232420603bef20b
alison <adevpr11@gmail.com>
2025-06-24 11:26:18 -0300

    feat: adiciona classe JwtBearerComponent

---
+++ src/main/java/br/com/cotiinformatica/components/JwtBearerComponent.java
@@ -0,0 +1,33 @@
+package br.com.cotiinformatica.components;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtBearerComponent {
+
+	@Value("${jwt.secretkey}")
+	private String jwtSecret;
+
+	@Value("${jwt.expiration}")
+	private String jwtExpiration;
+
+	public Date getExpiration() {
+		var currentDate = new Date();
+		return new Date(currentDate.getTime() + Integer.parseInt(jwtExpiration));
+	}
+
+	public String getToken(String emailUsuario) {
+		return Jwts.builder()
+				.setSubject(emailUsuario)
+				.setIssuedAt(new Date())
+				.setExpiration(getExpiration())
+				.signWith(SignatureAlgorithm.HS256, jwtSecret)
+				.compact();
+	}
+}
