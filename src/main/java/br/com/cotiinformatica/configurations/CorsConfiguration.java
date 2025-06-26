0364796015982a595dc38f455b91ea5a7b416361
alison <adevpr11@gmail.com>
2025-06-26 12:50:15 -0300

    fix: adiciona anotação na classe CorsConfiguration

--- src/main/java/br/com/cotiinformatica/configurations/CorsConfiguration.java
+++ src/main/java/br/com/cotiinformatica/configurations/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.cotiinformatica.configurations;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
 	public void addCorsMappings(CorsRegistry corsRegistry) {
