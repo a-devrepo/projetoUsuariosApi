d0c044da5836acd8d549761352f7453366551e29
alison <adevpr11@gmail.com>
2025-07-15 11:12:37 -0300

    refactor: altera formato dos campos de data nos DTOs

--- src/main/java/br/com/cotiinformatica/dtos/AutenticarUsuarioResponseDTO.java
+++ src/main/java/br/com/cotiinformatica/dtos/AutenticarUsuarioResponseDTO.java
@@ -3,6 +3,8 @@ package br.com.cotiinformatica.dtos;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,7 +13,9 @@ public class AutenticarUsuarioResponseDTO {
 	private UUID id;
 	private String nome;
 	private String email;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime dataHoraAcesso;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime dataHoraExpiracao;
 	private String token;
 }
--- src/main/java/br/com/cotiinformatica/dtos/CriarUsuarioResponseDTO.java
+++ src/main/java/br/com/cotiinformatica/dtos/CriarUsuarioResponseDTO.java
@@ -3,6 +3,8 @@ package br.com.cotiinformatica.dtos;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -11,5 +13,6 @@ public class CriarUsuarioResponseDTO {
 	private UUID id;
 	private String nome;
 	private String email;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime dataHoraCriacao;
 }
