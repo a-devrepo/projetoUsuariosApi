fcfc2a83acb8e113a6822470f40eecbd8aac889a
alison <adevpr11@gmail.com>
2025-08-01 01:51:33 -0300

    docs: adiciona documentação Swagger ao controller de usuários

--- src/main/java/br/com/cotiinformatica/controllers/UsuariosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/UsuariosController.java
@@ -10,10 +10,15 @@ import br.com.cotiinformatica.dtos.AutenticarUsuarioResponseDTO;
 import br.com.cotiinformatica.dtos.CriarUsuarioRequestDTO;
 import br.com.cotiinformatica.dtos.CriarUsuarioResponseDTO;
 import br.com.cotiinformatica.services.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
+@Tag(name = "Controle de usuários", description = "Serviços para gerenciamento de usuários.")
 public class UsuariosController {
 
 	private UsuarioService usuarioService;
@@ -23,12 +28,25 @@ public class UsuariosController {
 	}
 
 	@PostMapping
+	@Operation(
+		    summary = "Cadastro de usuários",
+		    description = "Cadastra um novo usuário no sistema"
+		)
+		@io.swagger.v3.oas.annotations.parameters.RequestBody(
+		    description = "Dados do usuário a ser cadastrado",
+		    required = true,
+		    content = @Content(
+		        mediaType = "application/json",
+		        schema = @Schema(implementation = CriarUsuarioResponseDTO.class)
+		    )
+		)
 	public CriarUsuarioResponseDTO criar(@RequestBody @Valid CriarUsuarioRequestDTO requestDTO) {
 
 		return usuarioService.criarUsuario(requestDTO);
 	}
 	
 	@PostMapping("/autenticar")
+	@Operation(summary = "Autenticar usuário", description = "Retorna as informações do usuário autenticado")
 	public AutenticarUsuarioResponseDTO autenticar(@RequestBody @Valid AutenticarUsuarioRequestDTO requestDTO) {
 
 		return usuarioService.autenticarUsuario(requestDTO);
